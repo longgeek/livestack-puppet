079b49c75f1097fa8c06c89c66bb0f0e6a4c36bc
longgeek <longgeek@gmail.com>
2014-05-09 22:44:53 +0800

    Icehouse, delete nova-network

--- puppet/modules/nova/manifests/services.pp
+++ puppet/modules/nova/manifests/services.pp
@@ -1,22 +1,8 @@
 class nova::services {
-    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-novncproxy", "nova-objectstore"]:
+    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-objectstore"]:
         ensure => running,
         enable => true,
         hasstatus => true,
         hasrestart => true,
-    }
-
-    if $enable_neutron == 'False' {
-        service { "nova-network":
-            ensure => running,
-            enable => true,
-            hasstatus => true,
-            hasrestart => true,
-        }
-    } else {
-        service { "nova-network":
-            ensure => stopped,
-            enable => false,
-        }
     }
 }
--- puppet/modules/nova/manifests/upstart.pp
+++ puppet/modules/nova/manifests/upstart.pp
@@ -24,6 +24,9 @@ class nova::upstart {
         "/etc/init/nova-xvpvncproxy.conf":
             source => 'puppet:///files/nova/init/nova-xvpvncproxy.conf';
 
+        "/etc/init/nova-spicehtml5proxy.conf":
+            source => 'puppet:///files/nova/init/nova-spicehtml5proxy.conf';
+
         "/etc/init/nova-objectstore.conf":
             source => 'puppet:///files/nova/init/nova-objectstore.conf',
     }
@@ -32,19 +35,19 @@ class nova::upstart {
         command => 'ln -s /lib/init/upstart-job /etc/init.d/nova-api && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-cert && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-compute && \
-                    ln -s /lib/init/upstart-job /etc/init.d/nova-network && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-conductor && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-scheduler && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-consoleauth && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-novncproxy && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-xvpvncproxy && \
                     ln -s /lib/init/upstart-job /etc/init.d/nova-objectstore && \
+                    ln -s /lib/init/upstart-job /etc/init.d/nova-spicehtml5proxy && \
                     update-rc.d nova-api defaults 66 && \
                     update-rc.d nova-cert defaults 67 && \
                     update-rc.d nova-scheduler defaults 68 && \
                     update-rc.d nova-conductor defaults 69 && \
                     update-rc.d nova-compute defaults 70 && \
-                    update-rc.d nova-network defaults 71 && \
+                    update-rc.d nova-spicehtml5proxy defaults 70 && \
                     update-rc.d nova-consoleauth defaults 71 && \
                     update-rc.d nova-novncproxy defaults 72 && \
                     update-rc.d nova-xvpvncproxy defaults 73 && \
