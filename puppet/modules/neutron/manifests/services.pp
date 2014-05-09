193e87ede2a42ad677226b7fdb9bb451dd0e15b7
longgeek <longgeek@gmail.com>
2014-05-09 19:52:35 +0800

    update icehouse config

--- puppet/modules/neutron/manifests/services.pp
+++ puppet/modules/neutron/manifests/services.pp
@@ -1,8 +1,13 @@
 class neutron::services {
-    service {["neutron-openvswitch-agent", "neutron-dhcp-agent", "neutron-l3-agent", "neutron-metadata-agent", "openvswitch-switch", "neutron-server", "neutron-metering-agent"]:
+    service {["neutron-openvswitch-agent", "neutron-dhcp-agent", "neutron-l3-agent", "neutron-metadata-agent", "openvswitch-switch", "neutron-server"]:
         ensure => running,
         enable => true,
         hasstatus => true,
         hasrestart => true,
+    }
+
+    service { "neutron-metering-agent":
+        ensure => stopped,
+        enable => false,
     }
 }
