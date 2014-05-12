dfb0348c54ad31384acd15b74faa400e8adc33b3
longgeek <longgeek@gmail.com>
2014-05-12 22:15:13 +0800

    update

--- puppet/modules/cinder/manifests/iscsi.pp
+++ puppet/modules/cinder/manifests/iscsi.pp
@@ -1,6 +1,11 @@
 class cinder::iscsi {
-    package { ["open-iscsi", "tgt", "lvm2"]:
+    package { ["open-iscsi", "tgt", "lvm2", "sysfsutils"]:
         ensure => installed,
+        notify => File["/etc/tgt/targets.conf"],
+    }
+
+    file { "/etc/tgt/targets.conf":
+        source => 'puppet:///files/cinder/etc/targets.conf',
         notify => Service["tgt", "open-iscsi"],
     }
 
--- puppet/modules/nova/manifests/services.pp
+++ puppet/modules/nova/manifests/services.pp
@@ -1,5 +1,5 @@
 class nova::services {
-    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-objectstore"]:
+    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-objectstore", "nova-novncproxy"]:
         ensure => running,
         enable => true,
         hasstatus => true,
