904cf515bc58058a3e7cb486a6699cf437ee98f7
longgeek <longgeek@gmail.com>
2014-05-15 14:05:50 +0800

    update spice & novnc

--- puppet/modules/nova/manifests/services.pp
+++ puppet/modules/nova/manifests/services.pp
@@ -1,8 +1,35 @@
 class nova::services {
-    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-objectstore", "nova-novncproxy"]:
+    service { ["nova-api", "nova-cert", "nova-scheduler", "nova-compute", "nova-conductor", "nova-consoleauth", "nova-objectstore"]:
         ensure => running,
         enable => true,
         hasstatus => true,
         hasrestart => true,
+    }
+
+    if $enable_spice == 'True' {
+        service { "nova-spicehtml5proxy":
+            ensure => running,
+            enable => true,
+            hasstatus => true,
+            hasrestart => true,
+        }
+
+        service { "nova-novncproxy":
+            ensure => stopped,
+            enable => false,
+        }
+
+    } else {
+        service { "nova-novncproxy":
+            ensure => running,
+            enable => true,
+            hasstatus => true,
+            hasrestart => true,
+        }
+
+        service { "nova-spicehtml5proxy":
+            ensure => stopped,
+            enable => false,
+        }
     }
 }
