0d579c71fee07bc49f905231a454b7b524025e8d
longgeek <longgeek@gmail.com>
2014-06-15 16:57:17 +0800

    Set swift log files

--- puppet/modules/swift/manifests/rsyslogs.pp
+++ puppet/modules/swift/manifests/rsyslogs.pp
@@ -4,6 +4,46 @@ class swift::rsyslogs {
         notify => Service['rsyslog'],
     }
 
+    file { '/var/log/swift/swift.log':
+        ensure => file,
+        owner  => 'syslog',
+        group  => adm,
+        mode   => 640,
+        notify => [ Service['rsyslog'], Class['swift::services']],
+    }
+
+    file { '/var/log/swift/proxy.log':
+        ensure => file,
+        owner  => syslog,
+        group  => adm,
+        mode   => 640,
+        notify => [ Service['rsyslog'], Class['swift::services']],
+    }
+
+    file { '/var/log/swift/account.log':
+        ensure => file,
+        owner  => syslog,
+        group  => adm,
+        mode   => 640,
+        notify => [ Service['rsyslog'], Class['swift::services']],
+    }
+
+    file { '/var/log/swift/object.log':
+        ensure => file,
+        owner  => syslog,
+        group  => adm,
+        mode   => 640,
+        notify => [ Service['rsyslog'], Class['swift::services']],
+    }
+
+    file { '/var/log/swift/container.log':
+        ensure => file,
+        owner  => syslog,
+        group  => adm,
+        mode   => 640,
+        notify => [ Service['rsyslog'], Class['swift::services']],
+    }
+
     service { 'rsyslog':
         ensure     => running,
         enable     => true,
